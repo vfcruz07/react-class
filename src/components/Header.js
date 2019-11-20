f09d1fabe19bc6fb658c347906bfedd39cff60e5
vfcruz07 <vfcruz07gmail.comgit config --global user.email vfcruz07@gmail.comgit config --global user.email vfcruz07gmail.com>
2019-11-20 16:49:22 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import{View, Text, StyleSheet} from 'react-native';
+
+
+const Header = (props) =>{
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}>People</Text>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 25,
+        backgroundColor: '#6ca2f7',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 50,
+        color: '#fff',
+            
+    }   
+
+});
+
+export default Header;
