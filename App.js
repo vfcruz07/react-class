9a34fd89c1f48c5fb95a60de09df838dc38476ca
vfcruz07 <vfcruz07gmail.comgit config --global user.email vfcruz07@gmail.comgit config --global user.email vfcruz07gmail.com>
2019-11-23 19:41:16 -0300

    aula 67 concluida - react navigation 4.0.10

--- App.js
+++ App.js
@@ -1,50 +1,15 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation';
 
-import Header from './src/components/Header.js';
-import PeopleList from './src/components/PeopleList.js';
+import PeoplePage from './src/pages/PeoplePage.js';
 
-import axios from 'axios';
-
-
-export default class App extends React.Component {
-  
-  constructor(props){
-    super(props);
-
-    this.state = {
-      peoples: []
-    }
-
+const StackNavigator = createStackNavigator({
+  "Main": {
+    screen: PeoplePage,
   }
-
-  componentDidMount(){
-    axios
-    .get('https://randomuser.me/api/?nat=br&results=5')
-    .then((response) => {
-      const { results } = response.data;
-      this.setState({
-        peoples: results
-      });
-    });
-  }
-
-  render(){
-    return (
-      <View>
-        <Header title='Pessoas 2!'></Header>
-        <PeopleList peoples={this.state.peoples}></PeopleList>
-      </View>
-    );  
-  }
-  
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
+
+const AppContainer = createAppContainer(StackNavigator);
+export default AppContainer;
+
+
---
+++ src/pages/PeoplePage.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Header from '../components/Header.js';
+import PeopleList from '../components/PeopleList.js';
+
+import axios from 'axios';
+
+
+export default class PeoplePage extends React.Component {
+  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      peoples: []
+    }
+
+  }
+
+  componentDidMount(){
+    axios
+    .get('https://randomuser.me/api/?nat=br&results=5')
+    .then((response) => {
+      const { results } = response.data;
+      this.setState({
+        peoples: results
+      });
+    });
+  }
+
+  render(){
+    return (
+      <View>
+        <Header title='Pessoas 2!'></Header>
+        <PeopleList peoples={this.state.peoples}></PeopleList>
+      </View>
+    );  
+  }
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
