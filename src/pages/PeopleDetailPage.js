648e2e65169833c7b90445ef03bcb9df58a24adf
Victor Cruz <vfcruz07@gmail.com>
2019-12-06 16:47:31 -0300

    Aula 73 parte 1

--- src/pages/PeopleDetailPage.js
+++ src/pages/PeopleDetailPage.js
@@ -1,13 +1,54 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
+
+
 
 
 export default class PeopleDetailPage extends React.Component{
     render(){
+
+        const { people } = this.props.navigation.state.params;
+
         return(
-            <View>
-                <Text>Eu sou a PeopleDetailPage !!</Text>
+            <View
+                style = {styles.container}
+            >
+                <Image 
+                    source ={{ uri: people.picture.large }}
+                    style={styles.avatar}
+                />
+                <View style = {styles.detailContainer}>
+                    <View style={styles.line}>
+                      <Text style={styles.cell}>Email: </Text>
+                      <Text style={styles.cell}>{people.email}</Text>
+                    </View>
+                    
+                </View>
             </View>
         );
     }
 }
+
+
+const styles = StyleSheet.create(
+    {
+        container:{
+            padding: 15,
+        },
+        avatar: {
+            aspectRatio: 1,
+        },
+        detailContainer:{
+            backgroundColor: '#e2f9ff',
+            marginTop: 20,
+            elevation: 1,
+        },
+        line:{
+
+        },
+        cell:{
+            
+        }
+        
+    }
+);
